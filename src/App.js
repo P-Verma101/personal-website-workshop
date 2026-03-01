471dda8a1eed0492e41059751ef14d6b1fa5d36d
Purnima <pverma@uvic.ca>
2026-02-28 16:43:56 -0800

    Changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import "./App.css";
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<h1>About Page</h1>} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
