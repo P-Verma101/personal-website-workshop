1fbbe1d622a5d7901e440b98136c296b3211dc75
Purnima <pverma@uvic.ca>
2026-05-23 12:08:13 -0700

    New Projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,25 @@ const Projects = () => {
             name: "Project Portfolio Website",
             description: "A personal website to showcase my projects, skills and experience.", 
             tech: "React, Bootstrap, React Router", 
-            link: "https://github.com/P-Verma101/project-portfolio"
+            link: "https://github.com/P-Verma101/personal-website-workshop"
         },
         {
             name: "Fraser Health x WEST MRI Stimulator Project",
             description: "The user interface design for the MRI Stimulator.", 
             tech: "Python, PySide, HTML, CSS", 
             link: "https://github.com/UVic-WEST/uvic-west-fraser-health-MRI-simulator"
+        },
+        {
+            name: "Simple Calculator",
+            description: "This was a simple calculator that performs simple calculations such as addition, subtraction, division and multiplication.",
+            tech: "Python",
+            link: "https://github.com/P-Verma101/Calculator"
+        },
+        {
+            name: "Snake Game",
+            description: "This was a simple snake game that I created using Python and the tiktok library. The game is a classic snake game where the player controls a snake that grows in length as it eats food. The objective of the game is to eat as much food as possible without colliding with the walls or the snake's own body.", 
+            tech: "Python, Git", 
+            link: "https://github.com/P-Verma101/Snake-Game"
         }
         
     ];
